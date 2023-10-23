f2a76af804089795d5e0a5685fc3ba3f3ff06cb9
noah <jaehoon.shin@konglabs.net>
2023-10-23 22:28:03 +0900

    jpa

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // DB 가 알아서 생성해주는 게 identity
     private  Long id;
+
+//    @Column(name)
     private  String name;
 
     public Long getId() {
